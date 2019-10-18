164328886f47d8837933dcb4b0c0c7281838278d
Alexis Triemstra <alexizt@yahoo.com.ar>
2019-10-18 11:03:02 +0200

    WebApp Initial Setup

---
+++ app/src/main/java/com/example/qrc/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.qrc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import 	android.graphics.drawable.ColorDrawable;
+import 	android.graphics.Color;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set content view AFTER ABOVE sequence (to avoid crash)
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#076dbb")));
+
+        getSupportActionBar().hide(); // hide the title bar
+        webview =(WebView)findViewById(R.id.webView);
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl("http://192.168.1.200:8081/Prelievo");
+    }
+}
